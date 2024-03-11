31c8a362fe60d7026a9d7848d4afd4573b1b8e3d
adrianmulyawan <adrianmulyawan666@gmail.com>
2024-03-11 13:03:27 +0700

    Add Feature : Midlleware check user email

---
+++ src/middleware/checkEmail.middleware.js
@@ -0,0 +1,24 @@
+const Model = require('../database/db/models');
+const User = Model.User;
+
+const checkUserEmail = async (req, res, next) => {
+  const { email } = req.body;
+
+  const isExsists = await User.findOne({
+    where: {
+      email: email
+    }
+  });
+
+  if (isExsists) {
+    return res.status(400).json({
+      status: 'Failed',
+      statusCode: 400,
+      message: 'Email has been used!'
+    });
+  }
+
+  next();
+};
+
+module.exports = checkUserEmail;
