040b02a75403d4edcaa33cad263bb7c311153348
adrianmulyawan <adrianmulyawan666@gmail.com>
2024-03-11 13:18:18 +0700

    edit check email middleware

--- src/middleware/checkEmail.middleware.js
+++ src/middleware/checkEmail.middleware.js
@@ -4,17 +4,19 @@ const User = Model.User;
 const checkUserEmail = async (req, res, next) => {
   const { email } = req.body;
 
-  const isExsists = await User.findOne({
+  const user = await User.findOne({
     where: {
       email: email
     }
   });
 
-  if (isExsists) {
+  // console.info(user.isActive);
+
+  if (user && user.isActive === false) {
     return res.status(400).json({
       status: 'Failed',
       statusCode: 400,
-      message: 'Email has been used!'
+      message: 'Email has been used and actived!'
     });
   }
 
