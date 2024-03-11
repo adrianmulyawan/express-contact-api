76a5bc9eaa8f48d315f5dcb3ebc097446385d61e
adrianmulyawan <adrianmulyawan666@gmail.com>
2024-03-11 12:54:25 +0700

    implement loggger

--- src/index.js
+++ src/index.js
@@ -2,14 +2,15 @@ const express = require('express');
 const cookieParser = require('cookie-parser');
 const path = require('path');
 require('dotenv').config();
-const morgan = require('morgan');
+const morganMiddleware = require('./middleware/morgan.middleware.js');
+const logger = require('./utils/logger.js');
 
 const indexRouter = require('./routes/index.route.js');
 
 const app = express();
 const port = +process.env.APP_PORT;
 
-app.use(morgan('dev'));
+app.use(morganMiddleware);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/images', express.static(path.join(__dirname, '../public/images')));
