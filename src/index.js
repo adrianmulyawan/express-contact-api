5c167a7698bafbdd147e87e9d52907ddf7b1c26e
adrianmulyawan <adrianmulyawan666@gmail.com>
2024-03-11 12:40:28 +0700

    implement morgan

--- src/index.js
+++ src/index.js
@@ -2,12 +2,14 @@ const express = require('express');
 const cookieParser = require('cookie-parser');
 const path = require('path');
 require('dotenv').config();
+const morgan = require('morgan');
 
 const indexRouter = require('./routes/index.route.js');
 
 const app = express();
 const port = +process.env.APP_PORT;
 
+app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/images', express.static(path.join(__dirname, '../public/images')));
