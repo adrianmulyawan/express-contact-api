72c02697a552d8b8cfa51cfeef3a07399883dec1
adrianmulyawan <adrianmulyawan666@gmail.com>
2024-03-18 14:21:55 +0700

    Add Feature : Update Data Contact and Address

--- src/controllers/contact.contoller.js
+++ src/controllers/contact.contoller.js
@@ -209,8 +209,139 @@ const getContactById = async (req, res) => {
   }
 };
 
+const updateContact = async (req, res) => {
+  try {
+    // > Tangkap data dari body
+    const { first_name, last_name, email, phone_number, addresses } = req.body;
+
+    const contactId = req.params.id;
+
+    const contact = await Contact.findOne({
+      where: {
+        id: contactId
+      },
+      include: {
+        model: Address,
+        as: 'addresses'
+      }
+    });
+
+    // > Validasi Body
+    if (!first_name || validator.isEmpty(first_name)) {
+      return res.status(400).json({
+        status: 'Failed',
+        statusCode: 400,
+        message: 'Error in First Name Field!',
+        error: 'First Name is Required!'
+      });
+    }
+
+    if (!last_name || validator.isEmpty(last_name)) {
+      return res.status(400).json({
+        status: 'Failed',
+        statusCode: 400,
+        message: 'Error in Last Name Field!',
+        error: 'Last Name is Required!'
+      });
+    }
+
+    if (!email || validator.isEmpty(email)) {
+      return res.status(400).json({
+        status: 'Failed',
+        statusCode: 400,
+        message: 'Error in Email Field!',
+        error: 'Email is Empty!'
+      });
+    }
+
+    if (!email || validator.isEmail(email) === false) {
+      return res.status(400).json({
+        status: 'Failed',
+        statusCode: 400,
+        message: 'Error in Email Field!',
+        error: 'Email not valid!'
+      });
+    }
+
+    if (phone_number) {
+      if (!validator.isMobilePhone(phone_number)) {
+        return res.status(400).json({
+          status: 'Failed',
+          statusCode: 400,
+          message: 'Error in Phone Number Field!',
+          error: 'Phone Number not valid!'
+        });
+      }
+    }
+
+    const fullname = first_name + " " + last_name;
+    if (!fullname || validator.isEmpty(fullname)) {
+      return res.status(400).json({
+        status: 'Failed',
+        statusCode: 400,
+        message: 'Error in Full Name Field!',
+        error: 'Full Name is Empty!'
+      });
+    }
+    
+    // > update data contact
+    const updateContact = await contact.update({
+      first_name: first_name || contact.first_name,
+      last_name: last_name || contact.last_name,
+      full_name: fullname || contact.full_name,
+      email: email || contact.email,
+      phone_number: phone_number || contact.phone_number
+    });
+
+    // > Bila terjadi kesalahan saat update data
+    if (!updateContact) {
+      return res.status(400).json({
+        status: 'Failed',
+        statusCode: 400,
+        message: 'Something error where update process!',
+      });
+    }
+
+    if (addresses && addresses.length > 0) {
+      const contact_id = contact.addresses[0].contact_id;
+      for (const address of addresses) {
+        await Address.update({ 
+          contact_id: updateContact.id,
+          address_type: address.address_type,
+          street: address.street,
+          city: address.city,
+          province: address.province,
+          country: address.country,
+          zip_code: address.zip_code
+        }, {
+          where: {
+            contact_id: contact_id
+          }
+        });
+      }
+    }
+
+    return res.status(201).json({
+      status: 'Success',
+      statusCode: 201,
+      message: 'Success Update Data Contact!',
+      dataContact: updateContact,
+      dataAddress: addresses,
+      error: []
+    });
+  } catch (error) {
+    return res.status(400).json({
+      status: 'Failed',
+      statusCode: 400,
+      message: 'Something Error in updateContact Controller!',
+      error: error.message
+    });
+  }
+};
+
 module.exports = {
   addNewContact,
   getContacts,
   getContactById,
+  updateContact,
 };
--- src/routes/contact.route.js
+++ src/routes/contact.route.js
@@ -4,11 +4,12 @@ const router = express.Router();
 const checkJWTToken = require('../middleware/checkToken.middleware.js');
 
 const { 
-  addNewContact, getContacts, getContactById 
+  addNewContact, getContacts, getContactById, updateContact 
 } = require('../controllers/contact.contoller');
 
 router.get('/contact', [checkJWTToken], getContacts);
 router.get('/contact/detail/:id', [checkJWTToken], getContactById);
 router.post('/contact/insert', [checkJWTToken], addNewContact);
+router.put('/contact/update/:id', [checkJWTToken], updateContact);
 
 module.exports = router;
